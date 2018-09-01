de91c2c8d1ae41888a7c6822c7144e5d017acd4d
Sidney Sousa <sidneysousa@MacBook-Pro-de-Sidney.local>
2018-08-31 20:57:42 -0400

    Adicionando o projeto aiquefome-client

---
+++ aiquefome-client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import bandeja from './bandeja.png';
+import talheres from './talheres.png';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+        <div id="banner">
+          <img id="logo" src={bandeja} alt="Bandeja" />
+          <h1>Ai, que fome!</h1>
+        </div>
+
+        <div className="container">
+
+          <div className="row">
+            <div id="conteudo" className="col">
+            </div>
+          </div>
+
+        </div>
+
+        <div id="rodape">
+          &copy;Sidney Sousa. Todos os direitos reservados.
+          &nbsp;
+          <img id="imgRodape" src={talheres} alt="Talheres" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ aiquefome-client/src/Restaurante.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Restaurante extends Component {
+
+    render() {
+        return (
+            <div>Restaurante</div>
+        );
+    }
+}
+
+export default Restaurante;
